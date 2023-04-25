d6d1fe9d1fad3c2f3c069471cbcf0c913b24a6fd
MuhammadEzraMaulana <126476343+MuhammadEzraMaulana@users.noreply.github.com>
2023-04-25 15:34:16 +0700

    Update piramida bintang.php

--- piramida bintang.php
+++ piramida bintang.php
@@ -2,11 +2,11 @@
     // variabel $tinggi di isi angka 9. Variabel inilah yang akan menentukan tinggi piramida bintang.
     $tinggi = 9;
 
-    //dimulai dari Looping variabel counter $i dipakai untuk membuat tinggi piramida.Ini dimulai dari$i = 1 $tinggi hingga $i >=
-    for ($i = $tinggi; $i >= 1; $i--) {
+    //dimulai dari Looping variabel counter $i dipakai untuk membuat tinggi piramida.Ini dimulai dari$i = 1 hingga $i >=
+    for ($i =1 ; $i<=10; $i++) {
 
-    //Looping variabel counter $j dipakai untuk membuat spasi yang akan mendorong karakter bintang ke arah kanan. Ini dimulai dari$i = 1 $tinggi hingga $i >=
-    for ($j = $i; $j >= 1; $j--) {
+    //Looping variabel counter $j dipakai untuk membuat spasi yang akan mendorong karakter bintang ke arah kanan. Ini dimulai dari$j = 10 hingga $j>$i
+    for ($j = 10; $j>$i; $j-=1) {
       
     // Mencetak bintang pada setiap kolom
     echo "*";
