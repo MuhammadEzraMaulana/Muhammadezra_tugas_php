7f92aff1c310efc8cbab4f92ec48e89977ba3d11
MuhammadEzraMaulana <126476343+MuhammadEzraMaulana@users.noreply.github.com>
2023-04-25 15:16:07 +0700

    Update piramida angka.php

--- piramida angka.php
+++ piramida angka.php
@@ -1,14 +1,14 @@
 <?php
-    // variabel $tinggi di isi angka 8. Variabel inilah yang akan menentukan tinggi piramida angka.
+    //variabel $tinggi di isi angka 8. Variabel inilah yang akan menentukan tinggi piramida angka.
     $tinggi = 8;
 
-    // Looping variabel counter $i dipakai untuk membuat tinggi piramida.Ini dimulai dari$i = 1 hingga $i <= $tinggi
+    //dimulai Looping variabel counter $i dipakai untuk membuat tinggi piramida.Ini dimulai dari$i = 1 hingga $i <= $tinggi
     for ($i = 0; $i <= $tinggi; $i++) {
 
-    // Looping variabel counter $j dipakai untuk membuat spasi yang akan mendorong karakter bintang ke arah kanan.Ini dimulai dari$j = 1   hingga $j <= $tinggi
+    //Looping variabel counter $j dipakai untuk membuat spasi yang akan mendorong karakter bintang ke arah kanan.Ini dimulai dari$j = 1   hingga $j <= $tinggi
     for ($j = 0; $j <= $i; $j++) {
         
-    // Mencetak angka pada setiap kolom
+    //Mencetak angka pada setiap kolom
     echo $j . " ";
  }
     // mencetak proses pindah baris
