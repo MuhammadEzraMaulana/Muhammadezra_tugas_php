0f847601881e6d1817050b87caf485eaa1d55203
MuhammadEzraMaulana <126476343+MuhammadEzraMaulana@users.noreply.github.com>
2023-05-15 19:16:27 +0700

    Update update.php

--- kelompok1/update.php
+++ kelompok1/update.php
@@ -1,13 +1,17 @@
 <?php
-
-    include "connect.php";
-    $id_kostumer = $_GET['id_kostumer'];
-    $tampil = mysqli_query($conn, "select * from kostumer where id_kostumer='$id_kostumer'");
-    $hasil = mysqli_fetch_array($tampil);
+include('connect.php');
+$id_kostumer = $_GET['id_kostumer'];
+$data_mobil = mysqli_query($conn, "SELECT * FROM kostumer where id_kostumer= '$id_kostumer'");
+while ($kostumer = mysqli_fetch_array($data_mobil)) {
+    $id_kostumer = $kostumer['id_kostumer'];
+    $nama_kostumer = $kostumer['nama_kostumer'];
+    $alamat_kostumer = $kostumer['alamat_kostumer'];
+}
 ?>
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -27,15 +31,15 @@
                         <form action="ubah.php" method="POST">
                             <div class="form-group">
                                 <label>ID KOSTUMER</label>
-                                <input type="text" name="id_kostumer" value="<?php echo $hasil['id_kostumer']; ?>" class="form-control" readonly>
+                                <input type="text" name="id_kostumer" value="<?php echo $id_kostumer; ?>" class="form-control" readonly>
                             </div>
                             <div class="form-group">
                                 <label>NAMA KOSTUMER</label>
-                                <input type="text" name="nama_kostumer" value="<?php echo $hasil['nama_kostumer']; ?>" class="form-control">
+                                <input type="text" name="nama_kostumer" value="<?php echo $nama_kostumer; ?>" class="form-control">
                             </div>
                             <div class="form-group">
                                 <label>Alamat KOSTUMER</label>
-                                <textarea class="form-control" name="alamat_kostumer" rows="4"><?php echo $hasil['alamat_kostumer']; ?></textarea>
+                                <textarea class="form-control" name="alamat_kostumer" rows="4"><?php echo $alamat_kostumer; ?></textarea>
                             </div>
                             <button type="submit" class="btn btn-success">UPDATE</button>
                             <a href="index.php" class="btn btn-secondary">Kembali</a>
@@ -47,4 +51,5 @@
     </div>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
+
 </html>
